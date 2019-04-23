6bc9fd153f1b4bc76145b44ba232dedd2032f04f
Josh Morris <jmo0624@gmail.com>
2019-04-23 00:45:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,4 +23,6 @@ def won?(board)
     index_1 = win_combo[1]
     index_2 = win_combo[2]
     
-    position_1 = board[index]
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
